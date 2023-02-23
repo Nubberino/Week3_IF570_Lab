374ea157a42600b19fe3b4ebee3f2820896b085d
Nubberino <ignatius.sarwono@student.umn.ac.id>
2023-02-23 11:06:20 +0700

    Adding class constructor
    engineer

--- example/myapp/Aquarium.kt
+++ example/myapp/Aquarium.kt
@@ -1,14 +1,32 @@
 package example.myapp
 
-class Aquarium {
-    var width: Int = 20;
-    var length: Int = 100;
-    var height: Int = 40;
+class Aquarium(length: Int = 100, width: Int = 20, height: Int = 40) {
+    init{
+        println("Aquarium, Innit?")
+    }
 
+
+
+    var width: Int = length;
+    var length: Int = width;
+    var height: Int = height;
+    var volume: Int
+        get() = width * height * length/ 1000
+        /* private */ set(value)
+        {
+            height = (value * 1000) / (width*length)
+        }
     fun printSize()
     {
         println("Witdh = $width cm " + "lenth = $length cm " + "hight = $height cm")
+        println("Volume = $volume litres")
+    }
 
+    constructor(Fishes: Int) : this()
+    {
+        val tank = Fishes * 2000 * 1.1
+
+        height = (tank/ (length * width)).toInt()
     }
 }
 
--- example/myapp/Main.kt
+++ example/myapp/Main.kt
@@ -2,10 +2,26 @@ package example.myapp
 
 fun buildAquarium()
 {
-    val myAquarium = Aquarium()
-    myAquarium.printSize()
-    myAquarium.height = 21
-    myAquarium.printSize()
+    val myAquarium1 = Aquarium()
+    myAquarium1.printSize()
+
+    val myAquarium2 = Aquarium(width = 3)
+    myAquarium2.printSize()
+
+    val myAquarium3 = Aquarium(height = 4)
+    myAquarium3.printSize()
+
+    val myAquarium4 = Aquarium(length = 21)
+    myAquarium4.printSize()
+
+    val myAquarium5 = Aquarium(Fishes = 21)
+    myAquarium5.printSize()
+
+    val myAquarium6 = Aquarium(Fishes = 29)
+    myAquarium6.printSize()
+    myAquarium6.volume = 70
+    myAquarium6.printSize()
+
 }
 
 fun main()
