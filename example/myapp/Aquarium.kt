bd7d8da5bcf253a83d234f40bd9079d1ad94773d
Nubberino <ignatius.sarwono@student.umn.ac.id>
2023-02-23 11:45:29 +0700

    Learning About Visibility Modifier
    Visi

--- example/myapp/Aquarium.kt
+++ example/myapp/Aquarium.kt
@@ -1,25 +1,28 @@
 package example.myapp
 
-class Aquarium(length: Int = 100, width: Int = 20, height: Int = 40) {
+import kotlin.math.PI
+
+open class Aquarium(open var length: Int = 100, open var width: Int = 20, open var height: Int = 40) {
     init{
         println("Aquarium, Innit?")
     }
 
-
-
-    var width: Int = length;
-    var length: Int = width;
-    var height: Int = height;
-    var volume: Int
+   open var volume: Int
         get() = width * height * length/ 1000
         /* private */ set(value)
         {
             height = (value * 1000) / (width*length)
         }
+
+    open val shape = "Recktangle"
+    open var water: Double = 0.0
+        get() = volume * 0.89
+
     fun printSize()
     {
+        println(shape)
         println("Witdh = $width cm " + "lenth = $length cm " + "hight = $height cm")
-        println("Volume = $volume litres")
+        println("Volume = $volume litres   water: $water litres (${water/volume * 100.0}% filled)")
     }
 
     constructor(Fishes: Int) : this()
@@ -27,6 +30,20 @@ class Aquarium(length: Int = 100, width: Int = 20, height: Int = 40) {
         val tank = Fishes * 2000 * 1.1
 
         height = (tank/ (length * width)).toInt()
+    }
+
+
+    class TowerTank(override var height: Int, var diameter: Int):Aquarium(height = height, width = diameter, length = diameter)
+    {
+        override var volume: Int
+            get() = (width/2 * length/2 * height/1000 * PI).toInt()
+            set(value)
+            {
+                height = ((value *1000 / PI) / (width/2 * length/2)).toInt()
+            }
+        override var water = volume * 0.77
+        override val shape = "Circular"
+
     }
 }
 
--- example/myapp/Main.kt
+++ example/myapp/Main.kt
@@ -1,26 +1,31 @@
 package example.myapp
+import java.lang.Math
 
 fun buildAquarium()
 {
-    val myAquarium1 = Aquarium()
+    val myAquarium1 = Aquarium(width = 25, length = 25, height = 40)
     myAquarium1.printSize()
+    val myTower = Aquarium.TowerTank(diameter = 25, height = 40)
+    myTower.printSize()
 
-    val myAquarium2 = Aquarium(width = 3)
-    myAquarium2.printSize()
+//
+//    val myAquarium2 = Aquarium(width = 3)
+//    myAquarium2.printSize()
+//
+//    val myAquarium3 = Aquarium(height = 4)
+//    myAquarium3.printSize()
+//
+//    val myAquarium4 = Aquarium(length = 21)
+//    myAquarium4.printSize()
+//
+//    val myAquarium5 = Aquarium(Fishes = 21)
+//    myAquarium5.printSize()
+//
+//    val myAquarium6 = Aquarium(length = 25, width = 25, height = 40)
+//    myAquarium6.printSize()
+//    myAquarium6.volume = 70
+//    myAquarium6.printSize()
 
-    val myAquarium3 = Aquarium(height = 4)
-    myAquarium3.printSize()
-
-    val myAquarium4 = Aquarium(length = 21)
-    myAquarium4.printSize()
-
-    val myAquarium5 = Aquarium(Fishes = 21)
-    myAquarium5.printSize()
-
-    val myAquarium6 = Aquarium(Fishes = 29)
-    myAquarium6.printSize()
-    myAquarium6.volume = 70
-    myAquarium6.printSize()
 
 }
 
